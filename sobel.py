de20c024515821f0dc4ca3942b2e9589e25dd72d
Ajay Kumar B R <44740988+ajaykumar-br@users.noreply.github.com>
2020-01-01 19:38:20 +0530

    Add files via upload

---
+++ sobel.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+def convolve_np(X,F):
+    X_height = X.shape[0]
+    X_width = X.shape[1]
+    
+    F_height = F.shape[0]
+    F_width = F.shape[1]
+    
+    H = int((F_height - 1)/2)
+    W = int((F_width - 1)/2)
+    
+    out = np.zeros((X_height, X_width))
+    
+    for i in np.arange(H, X_height-H):
+        for j in np.arange(W, X_width-W):
+            sum = 0
+            for k in np.arange(-H, H+1):
+                for l in np.arange(-W, W+1):
+                    a = X[i+k, j+l]
+                    w = F[H+k, W+l]
+                    sum += (w * a)
+            out[i, j] = sum
+    return out
+
+img = cv2.imread('image/image.jpg', 0)
+
+height = img.shape[0]
+width = img.shape[1]
+
+Hx = np.array([[-1, 0, 1],
+               [-2, 0, 2],
+               [-1, 0, 1]])
+    
+Hy = np.array([[-1, -2, -1],
+               [0, 0, 0],
+               [1, 2, 1]])
+
+img_x = convolve_np(img, Hx) / 8.0
+img_y = convolve_np(img, Hy) / 8.0
+
+img_out = np.sqrt(np.power(img_x, 2) + np.power(img_y, 2))
+
+img_out = (img_out / np.max(img_out)) * 255
+
+cv2.imwrite('image/Edge_Sobel.jpg', img_out)
+
+plt.imshow(img_out, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])
+plt.show()
